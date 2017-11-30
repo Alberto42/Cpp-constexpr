b8d6bcc9ca059575e166eeaf3a547c2f6fb54397
qazswedcx <33523701+qazswedcx@users.noreply.github.com>
2017-11-30 17:28:11 +0100

    SSSSS

--- pie.h
+++ pie.h
@@ -29,10 +29,10 @@ class Pie {
     
     public:
     
-    typedef std::conditional<forsale, std::true_type, std::false_type> Sellable;
-    typedef std::conditional<forsale, P, std::false_type> PriceType;
+    typedef std::conditional<forSale, std::true_type, std::false_type> Sellable;
+    typedef std::conditional<forSale, P, std::false_type> PriceType;
     typedef T SizeType;
-    typedef std::conditional<forsale, std::true_type, std::false_type> IsApplePie;
+    typedef std::conditional<forSale, std::true_type, std::false_type> IsApplePie;
     
     template<class U = P, class = class std::enable_if<!forSale && std::is_same<U,P>::value, U>::type>
     Pie(int stock): _stock(stock)
